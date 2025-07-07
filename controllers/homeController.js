b10d9e7ca2bb7fd9dda42f60f2a4c810f00a5b88
Swapnil Charjan <swapnilcharjan00@gmail.com>
2025-07-07 18:27:16 +0530

    CRUD controller changes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,17 +3,40 @@ import Home from '../modules/home.js';
 export const createHome = async (req, res) => {
     try {
         const HomeData = await Home.create(req.body);
-        res.status(201).json({HomeData, message: "Home created successfully"});
-    } catch (error) {   
-        res.status(500).json({error: error.message});
+        res.status(201).json({ HomeData, message: "Home created successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
 export const getAllHomes = async (req, res) => {
+    try {
+        const HomeList = await Home.find({});
+        res.status(200).json(HomeList);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
-export const editHome = (req, res) => {
+export const updateHome = (req, res) => {
+    try{
+        const { id } = req.params;
+        const updatedData = req.body;
+        Home.findByIdAndUpdate(id, updatedData, { new: true })        
+            .then(() => res.status(200).json({ message: "Home deleted successfully" }))
+            .catch(error => res.status(500).json({ error: error.message }));
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 export const deleteHome = (req, res) => {
+    try {
+        const { id } = req.params;
+        Home.findByIdAndDelete(id)
+            .then(() => res.status(200).json({ message: "Home deleted successfully" }))
+            .catch(error => res.status(500).json({ error: error.message }));
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -5,7 +5,7 @@ const homeController = require('../controllers/homeController');
 
 router.get('/all-homes', homeController.getAllHomes);
 router.post('/add-home', homeController.createHome);
-router.put('/edit-home/:id', homeController.editHome);
+router.put('/edit-home/:id', homeController.updateHome);
 router.delete('/delete-home/:id', homeController.deleteHome);
 
 module.exports = router;
